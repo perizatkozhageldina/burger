151487ab24fbb48c1c1a4198fcb1ca214d907301
Perizat Kozhageldina <perizat.kozhageldina@gmail.com>
2019-09-06 17:39:20 +0600

    accordeon sections done with js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//>>> pop-up menu
 let popupMenu = document.querySelector('#popup-menu');
 let openIcon = document.querySelector('#hamburger-menu');
 let closeIcon = document.querySelector('#close');
@@ -5,7 +6,7 @@ let menuList = document.querySelector('#popup-menu__list');
 
 openIcon.addEventListener("click", function(e) {
     e.preventDefault();
-    document.body.className = "disable-scrolling";
+    document.body.classList.add("disable-scrolling");
     popupMenu.style.display = "block";
 });
 
@@ -24,3 +25,26 @@ menuList.addEventListener("click", function(e){
         closeMenu();
     }
 });
+//<<< pop-up menu
+
+//>>> team section
+let membersItem = document.querySelectorAll('#members__item');
+
+for (let i=0; i<membersItem.length; i++) {
+    membersItem[i].addEventListener("click", function(e){
+    e.preventDefault();
+    membersItem[i].classList.toggle("members__item--active");
+    });
+}
+
+//<<< team section
+//>>> menu section
+let foodItem = document.querySelectorAll('#food__item');
+
+for (let i=0; i<foodItem.length; i++) {
+    foodItem[i].addEventListener("click", function(e){
+        e.preventDefault();
+        foodItem[i].classList.toggle("food__item--active");
+    });
+}
+//<<< menu section
