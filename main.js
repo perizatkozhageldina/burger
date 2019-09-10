ca43c02cba3c16ff3c7061fd2419f5715a857bc4
Perizat Kozhageldina <perizat.kozhageldina@gmail.com>
2019-09-10 10:12:09 +0600

    accordeons done with target element

--- main.js
+++ main.js
@@ -28,23 +28,114 @@ menuList.addEventListener("click", function(e){
 //<<< pop-up menu
 
 //>>> team section
-let membersItem = document.querySelectorAll('#members__item');
 
-for (let i=0; i<membersItem.length; i++) {
-    membersItem[i].addEventListener("click", function(e){
-    e.preventDefault();
-    membersItem[i].classList.toggle("members__item--active");
-    });
-}
+let teamSection = () => {
+    let membersName = document.querySelectorAll('.members__name');
 
+    membersName.forEach(function(membersLink) {
+        membersLink.addEventListener("click", function(e) {
+            e.preventDefault();
+            let activeMember = document.querySelector(".members__item--active");
+
+            if (activeMember) {
+                let membersDesc = activeMember.querySelector(".members__desc");
+                membersDesc.style.height = "0px";
+                activeMember.classList.remove("members__item--active");
+            }
+
+            if (!activeMember || activeMember.querySelector(".members__name") !== e.target) {
+                let currentMember = e.target.closest(".members__item");
+                currentMember.classList.add("members__item--active");
+                let currentMemberDesc = currentMember.querySelector(".members__desc");
+                currentMemberDesc.style.height = currentMemberDesc.scrollHeight + "px";
+            }
+        })
+    })
+};
+teamSection();
 //<<< team section
 //>>> menu section
-let foodItem = document.querySelectorAll('#food__item');
 
-for (let i=0; i<foodItem.length; i++) {
-    foodItem[i].addEventListener("click", function(e){
+let menuSection = () => {
+    let menuList = document.querySelector('.food__list');
+
+    menuList.addEventListener('click', e => {
         e.preventDefault();
-        foodItem[i].classList.toggle("food__item--active");
-    });
-}
+        let target = e.target;
+        const item = target.closest(".food__item");
+        const items = document.querySelectorAll(".food__item");
+        if (target.className === 'food__title') {
+            if (!item.classList.contains('food__item--active')) {
+                for (let i=0; i<items.length; i++) {
+                    items[i].classList.remove("food__item--active");
+                }
+                item.classList.add("food__item--active");
+            } else {
+                item.classList.remove("food__item--active");
+            }
+        }
+    })
+};
+menuSection();
+
+// let foodItem = document.querySelectorAll('#food__item');
+
+// for (let i=0; i<foodItem.length; i++) {
+//     foodItem[i].addEventListener("click", function(e){
+//         e.preventDefault();
+//         foodItem[i].classList.toggle("food__item--active");
+//     });
+// }
 //<<< menu section
+//>>> form section
+
+const myForm = document.querySelector('#myForm');
+const orderButton = document.querySelector('#orderButton');
+
+orderButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (validateForm(myForm)) {
+        const data = {
+            name: myForm.elements.name.value,
+            phone: myForm.elements.phone.value,
+            comment: myForm.elements.comment.value
+        };
+
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        console.log(xhr);
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send(JSON.stringify(data));
+        xhr.addEventListener ('onload', () => {
+            if (xhr.response.status == true) {
+                console.log('Все ок!');
+            };
+        });
+    }
+
+    function validateForm(form) {
+        let valid = true;
+
+        if (!validateField(form.elements.name)) {
+            valid = false;
+        }
+
+        if (!validateField(form.elements.phone)) {
+            valid = false;
+        }
+
+        if (!validateField(form.elements.comment)) {
+            valid = false;
+        }
+        return valid;
+    }
+
+    function validateField(field) {
+        field.nextElementSibling.textContent = field.validationMessage;
+        return field.checkValidity();
+    }
+
+});
+
+//<<< form section
