a7fd7d38a9245cfb62c19c3697499fd321c2cc31
Perizat Kozhageldina <perizat.kozhageldina@gmail.com>
2019-09-13 17:36:20 +0600

    how we work section added

--- main.js
+++ main.js
@@ -274,25 +274,103 @@ orderButton.addEventListener('click', function(e) {
 
             coloringDots(activeIndex); 
 
-            if (deltaY > 0) {
-                if (nextSection.length) {
-                    $this.animate({
-                        'top' : -nextIndex*100 + '%'
-                    }, duration, function(){
-                        activeSection.removeClass('active');
-                        nextSection.addClass('active');
-                    });
-                };
-            } else {
-                if (prevSection.length) {
-                    $this.animate({
-                        'top' : -prevIndex*100 + '%'
-                    }, duration, function(){
-                        activeSection.removeClass('active');
-                        prevSection.addClass('active');
-                    });
-                };
-            };  
+        if (deltaY > 0) {
+            if (nextSection.length) {
+                $this.animate({
+                    'top' : -nextIndex*100 + '%'
+                }, duration, function(){
+                    activeSection.removeClass('active');
+                    nextSection.addClass('active');
+                });
+            };
+        } else {
+            if (prevSection.length) {
+                $this.animate({
+                    'top' : -prevIndex*100 + '%'
+                }, duration, function(){
+                    activeSection.removeClass('active');
+                    prevSection.addClass('active');
+                });
+            };
+        };  
     });
 });
 //<<<one page scroll
+//>>> contacts
+
+ymaps.ready(init);
+
+var placemarks = [
+    {
+        latitude: 59.97,
+        longitude: 30.31,
+        hintContent: '<div class="map__hint">ул.Литераторов д.19</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger--img" src="/content/main/burger.png" style="max-height: 100px;" alt="Burger"/>',
+            'Самые вкусные бургеры у нас! </br>Заходите по адресу: </br>ул.Литераторов д.19',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.94,
+        longitude: 30.25,
+        hintContent: '<div class="map__hint">Малый проспект В О, д.64</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger--img" src="/content/main/burger.png" style="max-height: 100px;" alt="Burger"/>',
+            'Самые вкусные бургеры у нас! </br>Заходите по адресу: </br>Малый проспект В О, д.64',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.93,
+        longitude: 30.34,
+        hintContent: '<div class="map__hint">наб. реки Фонтанки д.19/div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger--img" src="/content/main/burger.png" style="max-height: 100px;" alt="Burger"/>',
+            'Самые вкусные бургеры у нас! </br>Заходите по адресу: </br>наб. реки Фонтанки д.19',
+            '</div>'
+        ]
+    }
+],
+    geoObjects = [];
+
+function init () {
+    var map = new ymaps.Map('map', {
+        center: [59.94, 30.32],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    for (i=0; i<placemarks.length; i++){
+            geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+            hintContent: placemarks[i].hintContent,
+            balloonContent: placemarks[i].balloonContent.join('')
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: '/content/svg/map-marker.svg',
+            iconImageSize: [46,57],
+            iconImageOffset: [-23,-57]
+        });
+    }
+
+    var clusterer = new ymaps.Clusterer({
+        clusterIcons: [
+            {
+                href: '/content/main/burger.png',
+                size: [100,100],
+                offset: [-50,-50]
+            }
+        ],
+        clusterIconContentLayout: null
+    });
+    
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
+    //map.geoObjects.add(placemark);
+}
+//<<< contacts
