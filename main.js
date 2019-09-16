44a79f8bba5406d43ecc1d9b3b403a295a5c7e06
Perizat Kozhageldina <perizat.kozhageldina@gmail.com>
2019-09-16 17:31:10 +0600

    video player added

--- main.js
+++ main.js
@@ -170,6 +170,100 @@ reviewsList.addEventListener('click', function(e) {
 });
 
 //<<< reviews section
+//>>> how we work section
+
+    let video;
+    let durationControl;
+    let soundControl;
+    let intervalId;
+    
+    video = document.getElementById("video");
+    play = document.getElementById("play");
+    playGray = document.getElementById("play-gray");
+    pause = document.getElementById("pause");
+
+    video.addEventListener('click', playStop);
+    play.addEventListener('click', playStop);
+    playGray.addEventListener('click', playStop);
+    pause.addEventListener('click', playStop);
+
+    let micControl = document.getElementById('sound');
+    micControl.addEventListener('click', soundOf);
+
+    durationControl = document.getElementById('regulator__line');
+    durationControl.addEventListener('mousedown', stopInterval);
+
+    durationControl.addEventListener('mouseup', setVideoDuration);
+
+    durationControl.min = 0;
+    durationControl.value = 0;
+
+    soundControl = document.getElementById('volume__line');
+    soundControl.addEventListener('mouseup', changeSoundVolume);
+
+    soundControl.min = 0;
+    soundControl.max = 10;
+
+    soundControl.value = soundControl.max;
+
+
+    video.addEventListener('ended', function () {
+        $(play).toggleClass("play--active");
+        video.currentTime = 0;
+    }, false);
+
+    function playStop() {
+        $(play).toggleClass("play--active");
+        $(playGray).toggleClass("play-gray--active");
+        $(pause).toggleClass("pause--active");
+        durationControl.max = video.duration;
+        
+        if(video.paused) {
+            video.play();
+            intervalId = setInterval(updateDuration, 1000/66)
+        } else {
+            video.pause();
+            clearInterval(intervalId);
+        }
+    }
+
+    function stopInterval () {
+        video.pause();
+        clearInterval(intervalId);
+    }
+
+    function setVideoDuration (){
+        video.currentTime = durationControl.value;
+        intervalId = setInterval(updateDuration, 1000/66);
+        
+        if (video.paused) {
+            video.play();
+            document.getElementsByClassName("play")[0].classList.add("play--active");
+        } else {
+            video.pause();
+        }
+    }
+
+    function updateDuration() {
+        durationControl.value = video.currentTime;
+    }
+
+    function soundOf () {
+        if (video.volume === 0) {
+            video.volume = soundLevel;
+            soundControl.value = soundLevel*10;
+        } else {
+            soundLevel = video.volume;
+            video.volume = 0;
+            soundControl.value = 0;
+        }
+    }
+
+    function changeSoundVolume () {
+        video.volume = soundControl.value/10;
+    }
+
+//<<< how we work section
 //>>> form section
 
 const myForm = document.querySelector('#myForm');
@@ -269,8 +363,6 @@ orderButton.addEventListener('click', function(e) {
             prevIndex = prevSection.index(),
             deltaY = e.originalEvent.deltaY,
             duration = 500;
-            
-            console.log(activeSection);
 
             coloringDots(activeIndex); 
 
